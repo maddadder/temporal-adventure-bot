56ac475fbf81201cca94ae8e94139ba396f57b3d
Charlie Lee <clee@tuta.io>
2022-05-22 02:23:02 -0700

    fix/use emojiSymbolToName

--- deploy-discord.example.sh
+++ deploy-discord.example.sh
@@ -1,4 +1,4 @@
 export DISCORD_BOT_TOKEN=the_DISCORD_BOT_TOKEN
 docker-compose build
-docker push 192.168.1.151:32000/temporal-adventure-bot:1.22.82
+docker push 192.168.1.151:32000/temporal-adventure-bot:1.22.92
 helm install temporal-discord-adventure-bot --set discordBotToken=$DISCORD_BOT_TOKEN --values ./temporal-adventure-bot/values.yaml --values ./temporal-adventure-bot/values.discord.yaml ./temporal-adventure-bot
